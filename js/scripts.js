8aa04a1b635e855697a91ec44996017ef75fb85a
nicholas mead and said ahmed <nicholas@>
2017-01-10 16:36:27 -0800

    add responses to conversation

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,14 @@ $(document).ready(function() {
   $("#meow").click(function() {
     $("#cat-col ul").append("<li>Meow!</li>");
     $("#dog-col ul").append("<li>I love you!</li>")
+    $("#cat-col ul").children("li").click(function() {
+      $("<img src='../img/cat.jpg' />").insertAfter();
+    });
   });
 
   $("#bark-bark").click(function() {
     $("#dog-col ul").append("<li>Bark Bark!</li>");
-    $("#cat-col ul").append("<li>Fuck Off!</li>")
+    $("#cat-col ul").append("<li>Fuck Off!</li>");
 
   });
 
